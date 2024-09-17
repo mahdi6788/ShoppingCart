c381da5c927498f8094b96fd71952125d30b42ea
mahdi <mjalalimech@gmail.com>
2024-09-17 16:01:52 +0400

    add pakages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function App() {
   
   return (
     <>
-      <h5>Store</h5>
+      <h5 className="text-success">Store</h5>
     </>
   )
 }
