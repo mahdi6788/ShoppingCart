ce119391171809b7300ba530bc28d5e9854955be
mahdi <mjalalimech@gmail.com>
2024-09-25 18:21:35 +0400

    add modal in navigationbar

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -1,19 +1,42 @@
-import { Navbar, Button } from "react-bootstrap"
+import { useState } from "react";
+import { Navbar, Button, Modal, ModalHeader } from "react-bootstrap"
 import { BsCart } from "react-icons/bs";
 
 
 function Navigationbar() {
+    const [showModal, setShowModal] = useState(false)
+
+    const handleOpenModal = () => {
+        setShowModal(true)
+    }
+    const handleCloseModal = () => {
+        setShowModal(false)
+    }
+
+
     return (
+        <>
         <Navbar className="border-bottom border-secondary">
             <Navbar.Collapse className="justify-content-end">
                 <Button 
                 variant="btn btn-outline-secondary"
-                className="text-white">
+                className="text-white"
+                onClick={handleOpenModal}>
                     <BsCart className="mx-1"></BsCart>                    
                     Add to Cart
                 </Button>
             </Navbar.Collapse>
         </Navbar>
+        <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header className="bg-black" closeButton >
+                <Modal.Title>Cart</Modal.Title>
+                <Modal.Body>Products</Modal.Body>
+                <Button variant="secondary" onClick={handleCloseModal}>
+                    Close
+                </Button>
+            </Modal.Header>
+        </Modal>
+        </>
     )
 }
 
