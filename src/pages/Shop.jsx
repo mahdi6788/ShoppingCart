d7244e0fc7542971cb59c7d4c0da753a47ee418b
mahdi <mjalalimech@gmail.com>
2024-09-25 15:10:19 +0400

    use map method

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,21 +1,17 @@
 import { Row, Col } from "react-bootstrap"
+import {productList} from "../data/items"
+import ProductItem from "../components/ProductItem"
 
 function Shop(){
     return (
         <Row xs={1} md={4} className="g-4">
-            <Col align="center">
-            Product
+            {productList.map(product => (
+            <Col 
+            align="center"
+            key={product.id}>
+            <ProductItem product={product} />
             </Col>
-            <Col align="center">
-            Product
-            </Col>
-            <Col align="center">
-            Product
-            </Col>
-            <Col align="center">
-            Product
-            </Col>
-            
+            ))}
         </Row>
     )
 }
