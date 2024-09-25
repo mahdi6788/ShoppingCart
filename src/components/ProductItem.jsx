307aa7dcd63e2550d7e3362ddd662b8d19cdd0c8
mahdi <mjalalimech@gmail.com>
2024-09-25 15:41:59 +0400

    edit size of card body and image
    h-100 for both card and cardImg helps them fill all content

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,18 +2,14 @@ import { Card, Button } from "react-bootstrap";
 
 function ProductItem({ product }) {
   return (
-    <Card className="mt-5 card-bg">
-      <Card.Body>
-        <Card.Img
-          variant="top"
-          src={product.image}
-          height="100%"
-        />
-        <Card.Title align="left" className="text-light pt-4">
+    <Card className="h-100 d-flex flex-column mt-5 card-bg">
+      <Card.Img variant="top h-100" src={product.image}/>
+      <Card.Body className="d-flex flex-column flex-grow-1 mt-auto">
+        <Card.Title align="left" className="text-light">
           {product.title}
         </Card.Title>
         <Card.Text align="left" className="text-light">
-          {product.price}
+          Price: ${product.price}
         </Card.Text>
         <Button variant="btn btn-outline-secondary" className="text-white">
           Add to cart
