bca1070ac128842573cbf8850c8d3cf80c993867
mahdi <mjalalimech@gmail.com>
2024-09-25 13:06:22 +0400

    add products

---
+++ src/data/items.js
@@ -0,0 +1,34 @@
+const productList = [
+    {
+        id: '1',
+        title: 'Product',
+        price: 99,
+        image: '/images/product.jpg'
+    },
+    {
+        id: '2',
+        title: 'Product',
+        price: 99,
+        image: '/images/product.jpg'
+    },
+    {
+        id: '3',
+        title: 'Product',
+        price: 99,
+        image: '/images/product.jpg'
+    },
+    {
+        id: '4',
+        title: 'Product',
+        price: 99,
+        image: '/images/product.jpg'
+    }
+]
+
+function getProductData(id) {
+    let productData = productList.find(product => product.id === id)
+
+    return productData
+}
+
+export {productList, getProductData}
