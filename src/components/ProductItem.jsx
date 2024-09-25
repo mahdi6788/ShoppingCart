1941e73b9bb4d29a39c462ef288a695a4a88f60f
mahdi <mjalalimech@gmail.com>
2024-09-25 14:15:25 +0400

    make jsx to put products into

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,31 @@
+import { Card, Button } from "react-bootstrap"
+
+function ProductItem ({product}){
+    return (
+        <Card className="mt-5 card-bg">
+            <Card.Body>
+                <Card.Img 
+                variant="top" 
+                src={product.image} 
+                height="200px"
+                style={{objectFit: "cover"}} />
+                <Card.Title 
+                align="right"
+                className="text-light pt-4">
+                    {product.title}
+                </Card.Title>
+                <Card.Text 
+                align="left"
+                className="'text-light">
+                    {product.price} 
+                </Card.Text>
+                <Button variant="btn btn-outline-secondary"
+                className="text-white">
+                    Add to cart
+                </Button>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default ProductItem
