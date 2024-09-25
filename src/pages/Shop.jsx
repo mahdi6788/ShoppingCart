7106a49d6a5bdda277e397064c25745cb68d7616
mahdi <mjalalimech@gmail.com>
2024-09-25 12:57:14 +0400

    make Shop page
    xs and md for determine the number of columns in two different size of page, small and medium

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,23 @@
+import { Row, Col } from "react-bootstrap"
+
+function Shop(){
+    return (
+        <Row xs={1} md={4} className="g-4">
+            <Col align="center">
+            Product
+            </Col>
+            <Col align="center">
+            Product
+            </Col>
+            <Col align="center">
+            Product
+            </Col>
+            <Col align="center">
+            Product
+            </Col>
+            
+        </Row>
+    )
+}
+
+export default Shop
