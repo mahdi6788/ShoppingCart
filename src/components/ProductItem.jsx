e5e383ed2b143c9d598f69e1c3e161e446e072e3
mahdi <mjalalimech@gmail.com>
2024-09-28 12:02:50 +0400

    import and use usecontext

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,14 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Row, Col, Form } from "react-bootstrap";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
 
 function ProductItem({ product }) {
+  const cart = useContext(CartContext);
+  const productQuantity = cart.getProductQuantity(product.id);
+
   return (
     <Card className="h-100 d-flex flex-column mt-5 card-bg">
-      <Card.Img variant="top h-100" src={product.image}/>
+      <Card.Img variant="top h-100" src={product.image} />
       <Card.Body className="d-flex flex-column flex-grow-1 mt-auto">
         <Card.Title align="left" className="text-light">
           {product.title}
@@ -11,9 +16,44 @@ function ProductItem({ product }) {
         <Card.Text align="left" className="text-light">
           Price: ${product.price}
         </Card.Text>
-        <Button variant="btn btn-outline-secondary" className="text-white">
-          Add to cart
-        </Button>
+        {productQuantity > 0 ? (
+          <>
+            <Form as={Row}>
+              <Col>
+                <Button
+                  onClick={() => cart.addItemToCart(product.id)}
+                  sm='6'
+                  className=" mx-2 text-white"
+                  variant="btn btn-outline-secondary">
+                  +
+                </Button>
+                <Form.Label sm="6" className="text-white">
+                {productQuantity}
+                </Form.Label >
+                <Button 
+                  onClick={() => cart.removeItemFromCart(product.id)}
+                  sm='6'
+                  className="mx-2 text-white"
+                  variant="btn btn-outline-secondary">
+                  -
+                </Button>
+              </Col>
+              </Form>
+              <Button  
+                onClick={() => cart.deleteFromCart(product.id)}
+                variant="btn btn-outline-warning mt-1">
+                Remove
+              </Button>
+          </>
+        ) : (
+          <Button
+            onClick={() => cart.addItemToCart(product.id)}
+            variant="btn btn-outline-secondary"
+            className="text-white"
+          >
+            Add to cart
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
